e40dadf61fccb4f9988b8179775428f9e7a8d75c
Nai-bo <naiboyu0515@gmail.com>
2026-04-04 22:02:49 +0800

    Update rating_dao_Naibo.py

--- DataBase/rating_dao_Naibo.py
+++ DataBase/rating_dao_Naibo.py
@@ -1,18 +1,24 @@
 from DataBase.db_connector import get_connection
 from database.sql_statements import EcommerceSQL
 
-#添加或更新评分
+# 添加或更新评分
 def add_or_update_rating(customer_id, vendor_id, score, conn=None):
     own_conn = conn is None
     conn = conn or get_connection()
     try:
         with conn.cursor() as cursor:
             cursor.execute(EcommerceSQL.ADD_RATING, (customer_id, vendor_id, score, score))
+        if own_conn:
+            conn.commit()
+    except Exception:
+        if own_conn:
+            conn.rollback()
+        raise
     finally:
         if own_conn:
             conn.close()
 
-# 获取一个供应商的所有评分
+# 获取某个供应商的所有评分
 def get_vendor_ratings(vendor_id, conn=None):
     own_conn = conn is None
     conn = conn or get_connection()
@@ -24,7 +30,7 @@ def get_vendor_ratings(vendor_id, conn=None):
         if own_conn:
             conn.close()
 
-# 获取某客户对某个供应商的评分
+# 获取客户对某个供应商的评分
 def get_customer_vendor_rating(customer_id, vendor_id, conn=None):
     own_conn = conn is None
     conn = conn or get_connection()
