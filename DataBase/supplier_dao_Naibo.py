b20c1e0519d9471b464b1cb10306cf2c0ce8aa7c
Nai-bo <naiboyu0515@gmail.com>
2026-04-04 21:04:55 +0800

    Update supplier_dao_Naibo.py

--- DataBase/supplier_dao_Naibo.py
+++ DataBase/supplier_dao_Naibo.py
@@ -1,6 +1,7 @@
 from DataBase.db_connector_Naibo import get_db_connection
 from database.sql_statements import EcommerceSQL
 
+# 获取所有供应商列表
 def get_all_suppliers():
     conn = get_db_connection()
     try:
@@ -9,7 +10,7 @@ def get_all_suppliers():
         return cursor.fetchall()
     finally:
         conn.close()
-
+# 添加供应商
 def add_supplier(business_name, geo_location):
     conn = get_db_connection()
     try:
@@ -18,7 +19,7 @@ def add_supplier(business_name, geo_location):
         conn.commit()
     finally:
         conn.close()
-
+# 根据ID获取供应商
 def get_supplier_by_id(supplier_id):
     conn = get_db_connection()
     try:
@@ -27,7 +28,7 @@ def get_supplier_by_id(supplier_id):
         return cursor.fetchone()
     finally:
         conn.close()
-
+# 根据供应商名获取供应商
 def search_suppliers_by_name(keyword):
     conn = get_db_connection()
     try:
