b5f29be18659de7d81779dcf9f70dbdfe8954592
Nai-bo <naiboyu0515@gmail.com>
2026-04-04 21:02:27 +0800

    Update customer_dao_Naibo.py

--- DataBase/customer_dao_Naibo.py
+++ DataBase/customer_dao_Naibo.py
@@ -1,6 +1,7 @@
 from DataBase.db_connector_Naibo import get_db_connection
 from database.sql_statements import EcommerceSQL
 
+#添加客户
 def add_customer(contact_num, shipping_address):
     conn = get_db_connection()
     try:
@@ -9,7 +10,7 @@ def add_customer(contact_num, shipping_address):
         conn.commit()
     finally:
         conn.close()
-
+# 根据ID查询顾客信息
 def get_customer_by_id(customer_id):
     conn = get_db_connection()
     try:
@@ -18,12 +19,22 @@ def get_customer_by_id(customer_id):
         return cursor.fetchone()
     finally:
         conn.close()
-
+# 更新顾客地址
 def update_customer_address(customer_id, shipping_address):
     conn = get_db_connection()
     try:
         cursor = conn.cursor()
         cursor.execute(EcommerceSQL.UPDATE_CUSTOMER_ADDRESS, (shipping_address, customer_id))
         conn.commit()
+    finally:
+        conn.close()
+
+# 验证顾客
+def validate_customer(customer_id):
+    conn = get_db_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute(EcommerceSQL.VALIDATE_CUSTOMER, (customer_id,))
+        return cursor.fetchone()
     finally:
         conn.close()
